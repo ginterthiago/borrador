647899d080f140709731414005db222e36a5118f
ginterthiago <ginterthiago@gmail.com>
2026-04-15 13:02:01 -0300

    Add files via upload

--- Introducción a programación tp1.py
+++ Introducción a programación tp1.py
@@ -2,24 +2,23 @@ TEXTO_NOMBRE_ALUMNO = "Ingrese el nombre del Alumno (): "
 TEXTO_EDAD_ALUMNO = "Ingrese la edad del Alumno (): "
 MENSAJE_ERROR_EDAD = "Error rn la Edad (): "
 
-alumno1_nombre = Entrada(TEXTO_NOMBRE_ALUMNO.Formato(1))
-Prueba:
-    alumno_edad = int(Entrada(TEXTO_EDAD_ALUMNO.Formato(1)))
+alumno1_nombre = input(TEXTO_NOMBRE_ALUMNO.format(1))
+try:
+   alumno_edad = int(input(TEXTO_EDAD_ALUMNO.format(1)))
+except ValueError:
+    print(MENSAJE_ERROR_EDAD.format(1))
+    alumno1_edad = "Error en Ingreso"  
 
-excepto ValueError: 
-      Impreso(MENSAJE_ERROR_EDAD.Formato(1))
-      alumno1_edad = "Error en Ingreso"  
-
-alumno2_nombre = Entrada(TEXTO_NOMBRE_ALUMNO.Formato(2))
-Prueba:
-   alumno2_edad = int(Entrada(TEXTO_EDAD_ALUMNO.Formato(2)))
-excepto ValueError: 
-    Impreso(MENSAJE_ERROR_EDAD.Formato(2))
+alumno2_nombre = input(TEXTO_NOMBRE_ALUMNO.format(2))
+try:
+   alumno2_edad = int(input(TEXTO_EDAD_ALUMNO.format(2)))
+except ValueError:
+    print(MENSAJE_ERROR_EDAD.format(2))
     alumno2_edad = "Error en Ingreso"
 
-alumno3_nombre = Entrada(TEXTO_NOMBRE_ALUMNO.Formato(3))
-Prueba:
-    alumno3_edad = int(Entrada(TEXTO_EDAD_ALUMNO.Formato(3)))
-excepto ValueError:
-   Impreso(MENSAJE_ERROR_EDAD.Formato(3))
+alumno3_nombre = input(TEXTO_NOMBRE_ALUMNO.format(3))
+try: 
+   alumno3_edad = int(input(TEXTO_EDAD_ALUMNO.format(3)))
+except ValueError: 
+    print(MENSAJE_ERROR_EDAD.format(3))
 alumno3_edad = "Error en Ingreso"
